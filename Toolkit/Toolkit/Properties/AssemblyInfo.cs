e8a1d3ae635ff847c76baa0109aa07407491889a
JustinPhlegar <Phlegar85@yahoo.com>
2015-12-07 23:42:56 -0500

    First test

--- Toolkit/Toolkit/Properties/AssemblyInfo.cs
+++ Toolkit/Toolkit/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("Toolkit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Orasi")]
 [assembly: AssemblyProduct("Toolkit")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
